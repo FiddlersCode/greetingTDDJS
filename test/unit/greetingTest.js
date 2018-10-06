1ddc123f4e8d00442f2d358ebbaf383e14241b35
Paula Muldoon <paula.muldoon@kurtgeiger.com>
2018-10-06 20:28:38 +0100

    add initial tests for greeting

---
+++ test/unit/greetingTest.js
@@ -0,0 +1,21 @@
+const chai = require('chai');
+
+const { expect } = chai;
+
+const Greeting = require('../../src/greeting');
+
+describe('Greeting', () => {
+    describe('constructor', () => {
+        it('should be an instance of the Greeting class', () => {
+            const testGreeting = new Greeting();
+            expect(testGreeting).to.be.instanceof(Greeting);
+        });
+    });
+
+    describe('single name', () => {
+        it('should return a simple greeting', () => {
+            const testGreeting = new Greeting();
+            expect(testGreeting.greet('Bob')).to.eq('Hello, Bob');
+        });
+    });
+});
